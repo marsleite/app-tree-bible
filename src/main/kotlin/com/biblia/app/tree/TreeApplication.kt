307c70f31d5ddeb2921d659c9b24d7fc81ca682c
Marcelo <marcelo.leite@mercadolivre.com>
2022-05-26 10:51:50 -0300

    first commit

---
+++ src/main/kotlin/com/biblia/app/tree/TreeApplication.kt
@@ -0,0 +1,11 @@
+package com.biblia.app.tree
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TreeApplication
+
+fun main(args: Array<String>) {
+	runApplication<TreeApplication>(*args)
+}
